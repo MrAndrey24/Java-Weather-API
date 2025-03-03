ac2b483840e87ec6f51edbdb4082ad7d6dd0de60
Andrey <andreyacostar@gmail.com>
2025-03-03 16:22:12 -0600

    first commit

---
+++ src/main/java/org/example/weatherapi/controller/WeatherController.java
@@ -0,0 +1,21 @@
+package org.example.weatherapi.controller;
+
+import org.example.weatherapi.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/weather")
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping("/{city}")
+    public String getWeather(@PathVariable String city) {
+        return  weatherService.getWeather(city);
+    }
+}
---
+++ src/main/java/org/example/weatherapi/service/WeatherService.java
@@ -0,0 +1,28 @@
+package org.example.weatherapi.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+
+    @Value("${weather.api.key}")
+    private String apiKey;
+
+    @Value("${weather.api.url}")
+    private String baseUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public String getWeather(String city) {
+        String url = String.format("%s/%s?unitGroup=metric&key=%s&contentType=json", baseUrl, city, apiKey);
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            return response.getBody();
+        }catch (Exception e) {
+            throw new RuntimeException("Error while fetching weather data" + e.getMessage());
+        }
+    }
+}
